ed6a994bc7d298c93be09f31e15c3787ed55e2ff
Khabbab Sarker <143183540+khabbabsarkar@users.noreply.github.com>
2025-03-04 16:23:25 +0600

    Add files via upload

---
+++ pdfmarger.py
@@ -0,0 +1,13 @@
+from PyPDF2 import PdfMerger
+
+ALLPDF = ["1.pdf", "2.pdf"]
+
+OurMerger j PdfMerger()
+
+for NewPDF in AllPDF:
+
+OurMerger.append(NewPDF)
+
+OurMerger.write("Pdf3.pdf")
+
+OurMerger.closer()
