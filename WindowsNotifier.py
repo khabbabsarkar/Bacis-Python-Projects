95667562cf4f821bf3e998a01813f9996a5bc435
Khabbab Sarker <143183540+khabbabsarkar@users.noreply.github.com>
2025-03-04 17:44:13 +0600

    Update WindowsNotifier.py

--- WindowsNotifier.py
+++ WindowsNotifier.py
@@ -3,4 +3,6 @@ from win10toast import ToastNotifier
 
 Toast = ToastNotifier()
 
-Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=10) Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=5 ) Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=1)
+Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=10) 
+Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=5) 
+Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=1)
