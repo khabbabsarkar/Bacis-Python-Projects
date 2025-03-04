36c124f2a8e13b1c2e3598d978bf3ba1f289795e
Khabbab Sarker <143183540+khabbabsarkar@users.noreply.github.com>
2025-03-04 16:02:01 +0600

    Add files via upload

---
+++ madlibsgame.py
@@ -0,0 +1,11 @@
+yourname = input("what is your name:")
+
+fathername = input("what is your father name:")
+
+mothername = input("what is your mother name")
+
+classname = input("which class do you read in:")
+
+relationship = input("tell me about your relationship status") I
+
+print (f"Dog Name Is {username}, My dogs father name {fatherName), my dogs mother name is {motherName} f"my do doggy study in class {study}, Our relationship is {relationship}")
