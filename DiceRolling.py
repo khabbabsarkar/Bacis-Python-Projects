07a6ed8a844728161b68ebc548163a5bc48bde05
Khabbab Sarker <143183540+khabbabsarkar@users.noreply.github.com>
2025-03-04 21:47:48 +0600

    Add files via upload

---
+++ DiceRolling.py
@@ -0,0 +1,15 @@
+DiceRolling = True
+
+while DiceRolling:
+
+print(random.randint(1,6)
+
+PlayAgain = input ("Want To Roll Again [y/n]:")
+
+if PlayAgain == "y":
+
+continue
+
+else:
+	 break
+print(''Game Over'')
