94b8d5c231f3f44bcfc24b77f378b5a549884149
Khabbab Sarker <143183540+khabbabsarkar@users.noreply.github.com>
2025-03-04 17:39:53 +0600

    Add files via upload

---
+++ WindowsNotifier.py
@@ -0,0 +1,6 @@
+#pip install win10toast
+from win10toast import ToastNotifier
+
+Toast = ToastNotifier()
+
+Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=10) Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=5 ) Toast.show_toast("Learning Time", "Hi Khabbab It's time To learn Programming", duration=1)
